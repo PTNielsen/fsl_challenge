79fe5f96b506a3feb4d90e2735f1278a092bdd54
Patrick Nielsen <ptnielsen55@gmail.com>
2017-09-19 16:03:45 -0400

    Downcase Person#email and Vehicle#license_plate before_save

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,5 +2,13 @@ class Person < ActiveRecord::Base
   belongs_to :household
   has_many :vehicles, foreign_key: :owner_id
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: false }
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,13 @@
 class Vehicle < ActiveRecord::Base
   belongs_to :owner, class_name: 'Person'
 
-  validates :license_plate, uniqueness: true
+  validates :license_plate, uniqueness: { case_sensitive: false }
+
+  before_save :downcase_license_plate
+
+  private
+
+  def downcase_license_plate
+    self.license_plate.downcase!
+  end
 end
