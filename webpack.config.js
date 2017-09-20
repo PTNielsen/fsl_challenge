c7ef4882567ad192a4840699110d5e69b070fccd
Patrick Nielsen <ptnielsen55@gmail.com>
2017-09-19 22:40:44 -0400

    Add config for webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+let webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: "inline_sourcemap",
+  entry: "./app/client/js/index.js",
+  output: {
+    path: __dirname + "/public/static",
+    filename: "index.min.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/,  loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['es2015', 'react'] } }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+  ]
+};
