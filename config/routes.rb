bde37e12676c4f926adc3acf52327f088e10e219
Patrick Nielsen <ptnielsen55@gmail.com>
2017-09-21 15:50:56 -0400

    Trim route resources and add summary

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :household, :person, :vehicle, only: [:index, :create, :edit]
+  resources :household, :person, :vehicle, only: [:create]
+  resources :summary, only: [:show]
 end
