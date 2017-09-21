ab1b9e08e0199ccab066cc65da15bd970a747c09
Patrick Nielsen <ptnielsen55@gmail.com>
2017-09-21 15:49:43 -0400

    Create class method to get household summary data

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,4 +1,12 @@
 class Household < ActiveRecord::Base
   has_many :residents, class_name: 'Person'
   has_many :vehicles, through: :residents
+
+  class << self
+    def summary_data id
+      household = includes(residents: :vehicles).find(id)
+      
+      return household.as_json(include: {residents: {include: :vehicles}})
+    end
+  end
 end
