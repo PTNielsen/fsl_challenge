4a1e9a74ed32e14b1ca94827e81b495ea9be9083
Patrick Nielsen <ptnielsen55@gmail.com>
2017-09-22 01:06:17 -0400

    Properly determine which component to show depending on state

--- client/components/WizardBox.jsx
+++ client/components/WizardBox.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
-import FormBox from './FormBox.jsx';
+import HouseholdForm from './HouseholdForm.jsx';
+import PersonForm from './PersonForm.jsx';
+import VehicleForm from './VehicleForm.jsx';
+import Summary from './Summary.jsx';
+import * as PAGES from '../constants/PageTypes.js';
 
 export default class WizardBox extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      households: []
-    };
-  }
-
-  componentWillMount() {
-    this._getHouseholds();
-  }
-
   render() {
+    const { wizard, actions } = this.props;
+    const component = this._determinePage(wizard.page, actions);
+
     return (
       <section>
-        <h1>YOOOOOOO dawg</h1>
-        <FormBox />
+        {component}
       </section>
     )
   }
 
-  _getHouseholds() {
-    fetch('/household', {
+  _determinePage(page, actions) {
+    switch (page) {
+      case (PAGES.HOUSEHOLD_PAGE):
+        return <HouseholdForm
+          actions={actions}
+          nextPage={PAGES.PERSON_PAGE} />;
+      case (PAGES.PERSON_PAGE):
+        return <PersonForm
+          actions={actions}
+          nextPage={PAGES.VEHICLE_PAGE}
+          householdId={this.props.wizard.householdId} />;
+      case (PAGES.VEHICLE_PAGE):
+        return <VehicleForm
+          actions={actions}
+          nextPage={PAGES.SUMMARY_PAGE} />;
+      case (PAGES.SUMMARY_PAGE):
+        return <Summary summary={this._getSummaryData()}/>;
+    }
+  }
+
+  _getSummaryData() {
+    const householdId = this.props.wizard.householdId;
+
+    fetch(`/household/${householdId}/summary`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -33,7 +49,7 @@ export default class WizardBox extends React.Component {
     }).then( (response) => {
       return response.json();
     }).then( (jsonData) => {
-      this.setState({households: jsonData});
+      console.log(jsonData);
     }).catch( (error) => {
       console.log('Error: ', error);
     });
