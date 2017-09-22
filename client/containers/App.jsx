a023e69cd7fe3b6bd830093dbde8673ba3ed6324
Patrick Nielsen <ptnielsen55@gmail.com>
2017-09-22 01:02:54 -0400

    Properly connect react and redux with container

---
+++ client/containers/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import WizardBox from '../components/WizardBox.jsx'
+import * as WizardActions from '../actions/wizardActions.js';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <section>
+        <WizardBox 
+          wizard={this.props.wizard}
+          actions={this.props.actions} />
+      </section>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    wizard: state
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(WizardActions, dispatch)
+  }
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(App)
