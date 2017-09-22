d06bed6101891aaed6f4c024347276232baf43c5
Patrick Nielsen <ptnielsen55@gmail.com>
2017-09-22 13:55:42 -0400

    Get summary data

--- client/components/Summary.jsx
+++ client/components/Summary.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 
-export default function Summary(props) {
-  console.log(props);
-  return(
-    <h1>Summary!</h1>
-  )
+export default class Summary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      summary: {}
+    }
+  }
+
+  componentWillMount() {
+    this._getSummaryData();
+  }
+
+  render() {
+    return(
+      <h1>Summary!</h1>
+    )
+  }
+
+  _getSummaryData() {
+    const householdId = this.props.householdId;
+    
+    fetch(`/household/${householdId}/summary`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json',
+      }
+    }).then( (response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error();
+      }
+    }).then( (jsonData) => {
+      this.setState({summary: jsonData});
+    }).catch( (error) => {
+      console.log('Error: ', error);
+    });
+  }
 }
