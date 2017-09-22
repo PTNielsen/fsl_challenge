85bf29b56570cc8455bb88bf15fda4ad7dd87424
Patrick Nielsen <ptnielsen55@gmail.com>
2017-09-22 01:04:34 -0400

    Clean up js/index.js after moving other pieces to their own directories

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../components/App.jsx';
+import React           from 'react';
+import { render }      from 'react-dom';
+import { createStore } from 'redux';
+import { Provider }    from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from '../containers/App.jsx';
+import wizardReducer from '../reducers/wizardReducer';
 
-console.log("In index.js");
+const store = createStore(wizardReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
+
+store.subscribe( () => {
+  console.log('Store updated: ', store.getState());
+});
