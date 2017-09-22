4cc4c1efa7a70a71b8937556bd5889dda3905d95
Patrick Nielsen <ptnielsen55@gmail.com>
2017-09-22 01:02:16 -0400

    Add constants in client directory

---
+++ client/constants/ActionTypes.js
@@ -0,0 +1,2 @@
+export const CHANGE_PAGE       = "CHANGE_PAGE";
+export const SET_HOUSEHOLD_ID  = "SET_HOUSEHOLD_ID";
---
+++ client/constants/PageTypes.js
@@ -0,0 +1,4 @@
+export const HOUSEHOLD_PAGE = 'HOUSEHOLD_PAGE';
+export const PERSON_PAGE    = 'PERSON_PAGE';
+export const VEHICLE_PAGE   = 'VEHICLE_PAGE';
+export const SUMMARY_PAGE   = 'SUMMARY_PAGE';
