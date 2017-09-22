89a0c4bab620fb3bc41a777d48671f09a1180b7b
Patrick Nielsen <ptnielsen55@gmail.com>
2017-09-22 01:34:23 -0400

    Add dependencies in order to include styling in components and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/,  loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['es2015', 'react'] } }
+      { test: /\.jsx?$/,  loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['es2015', 'react'] } },
+      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] }
     ]
   },
   plugins: [
